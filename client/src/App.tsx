127b3759135be3ff2fe5f2334f426147b31b743a
hzokbe <hzokbe@gmail.com>
2025-08-12 17:08:46 -0300

    feat(app): update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,13 @@
 import "./App.css";
 
+import { BrowserRouter as Router, Routes } from "react-router-dom";
+
 function App() {
-  return <></>;
+  return (
+    <Router>
+      <Routes></Routes>
+    </Router>
+  );
 }
 
 export default App;
