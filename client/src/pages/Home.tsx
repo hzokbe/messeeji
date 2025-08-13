0efba707741eca81c0a5c28557e7a5d9388d8772
hzokbe <hzokbe@gmail.com>
2025-08-13 20:57:27 -0300

    feat(home): add bottom navigation bar

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,8 +1,31 @@
-import { Box, Typography } from "@mui/material";
+import {
+  HouseOutlined,
+  PeopleOutlined,
+  SettingsOutlined,
+} from "@mui/icons-material";
+
+import {
+  BottomNavigation,
+  BottomNavigationAction,
+  Box,
+  Typography,
+} from "@mui/material";
+
+import { useState } from "react";
 
 const Home = () => {
+  const [index, setIndex] = useState(0);
+
   return (
-    <Box sx={{ maxWidth: 400, mx: "auto", mt: 4 }}>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100dvh",
+      }}
+    >
       <Typography
         variant="h2"
         component="h1"
@@ -13,10 +36,30 @@ const Home = () => {
           textAlign: "center",
           userSelect: "none",
           fontWeight: "bolder",
+          color: "#a3a3a3",
         }}
       >
         メッセージ
       </Typography>
+
+      <BottomNavigation
+        showLabels
+        value={index}
+        onChange={(_, newIndex) => {
+          setIndex(newIndex);
+        }}
+        sx={{
+          width: "100vw",
+          position: "absolute",
+          bottom: "0px",
+        }}
+      >
+        <BottomNavigationAction label="Home" icon={<HouseOutlined />} />
+
+        <BottomNavigationAction label="Groups" icon={<PeopleOutlined />} />
+
+        <BottomNavigationAction label="Settings" icon={<SettingsOutlined />} />
+      </BottomNavigation>
     </Box>
   );
 };
