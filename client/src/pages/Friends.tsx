a467bce9a7a7515e626063ba153b659cb9695772
hzokbe <hzokbe@gmail.com>
2025-08-15 22:08:13 -0300

    feat(friends): improve friends page adding friends list

--- client/src/pages/Friends.tsx
+++ client/src/pages/Friends.tsx
@@ -1,4 +1,12 @@
-import { Box, Typography } from "@mui/material";
+import {
+  Avatar,
+  Box,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  Typography,
+} from "@mui/material";
 
 import { useEffect, useState } from "react";
 
@@ -10,12 +18,31 @@ import setIndexByPathname from "../utils/location";
 
 import NavigationBar from "../components/NavigationBar";
 
+type Friend = {
+  name: string;
+  username: string;
+  avatarSrc: string;
+};
+
 const Friends = () => {
   const [index, setIndex] = useState(0);
 
   const navigate = useNavigate();
 
   const location = useLocation();
+
+  const friends: Friend[] = [
+    {
+      name: "Alan Turing",
+      username: "alan_turing",
+      avatarSrc: "static/images/avatar/alan-turing.jpg",
+    },
+    {
+      name: "Linus Torvalds",
+      username: "linus_torvalds",
+      avatarSrc: "static/images/avatar/linus-torvalds.jpg",
+    },
+  ];
 
   useEffect(
     () => setIndexByPathname(location.pathname, setIndex),
@@ -28,28 +55,49 @@ const Friends = () => {
   return (
     <Box
       sx={{
+        boxSizing: "border-box",
         display: "flex",
         flexDirection: "column",
         alignItems: "center",
-        justifyContent: "center",
+        justifyContent: "start",
         height: "100dvh",
+        padding: "8px",
       }}
     >
-      <Typography
-        variant="h2"
-        component="h1"
+      <List
         sx={{
-          whiteSpace: "nowrap",
-          overflow: "hidden",
-          textOverflow: "ellipsis",
-          textAlign: "center",
-          userSelect: "none",
-          fontWeight: "bolder",
-          color: "#a3a3a3",
+          width: "100%",
         }}
       >
-        Friends
-      </Typography>
+        {friends.map((f: Friend) => (
+          <ListItem
+            key={`${f.username}`}
+            alignItems="flex-start"
+            sx={{
+              cursor: "pointer",
+              transition: "background-color 0.25s ease-in-out",
+              "&:hover": { backgroundColor: "#f5f5f5" },
+            }}
+          >
+            <ListItemAvatar>
+              <Avatar alt={f.name} src={f.avatarSrc} />
+            </ListItemAvatar>
+            <ListItemText
+              primary={
+                <Typography
+                  sx={{
+                    color: "#525252",
+                    fontWeight: "bold",
+                    userSelect: "none",
+                  }}
+                >
+                  {f.name}
+                </Typography>
+              }
+            />
+          </ListItem>
+        ))}
+      </List>
 
       <NavigationBar index={index} onChange={onChange} />
     </Box>
