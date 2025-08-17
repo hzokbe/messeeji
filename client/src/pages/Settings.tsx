bc3ab78c955b6a4313635d0a56630ebd1fa16890
hzokbe <hzokbe@gmail.com>
2025-08-17 14:12:28 -0300

    feat(settings): update settings page adding user info

--- client/src/pages/Settings.tsx
+++ client/src/pages/Settings.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from "@mui/material";
+import { Avatar, Box, ButtonBase, Typography } from "@mui/material";
 
 import { useEffect, useState } from "react";
 
@@ -10,12 +10,20 @@ import setIndexByPathname from "../utils/location";
 
 import NavigationBar from "../components/NavigationBar";
 
+import type { User } from "../types/user";
+
 const Settings = () => {
   const [index, setIndex] = useState(2);
 
   const navigate = useNavigate();
 
   const location = useLocation();
+
+  const user: User = {
+    name: "Lucas",
+    lastName: "Fernandes",
+    status: "online",
+  };
 
   useEffect(
     () => setIndexByPathname(location.pathname, setIndex),
@@ -24,6 +32,24 @@ const Settings = () => {
 
   const onChange = (_: any, newIndex: number) =>
     handleNavigationChange(newIndex, setIndex, navigate);
+
+  const [avatarSrc, setAvatarSrc] = useState<string>(
+    "static/images/avatar/me.png"
+  );
+
+  const handleAvatarChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+
+    if (file) {
+      const reader = new FileReader();
+
+      reader.onload = () => {
+        setAvatarSrc(reader.result as string);
+      };
+
+      reader.readAsDataURL(file);
+    }
+  };
 
   return (
     <Box
@@ -39,26 +65,90 @@ const Settings = () => {
         sx={{
           flex: "1",
           display: "flex",
-          alignContent: "center",
+          alignItems: "center",
           justifyContent: "center",
           flexDirection: "column",
           width: "100%",
+          gap: "8px",
         }}
       >
-        <Typography
-          variant="h2"
-          component="h1"
+        <ButtonBase
+          component="label"
+          role={undefined}
+          tabIndex={-1}
+          aria-label="Avatar image"
           sx={{
-            whiteSpace: "nowrap",
-            overflow: "hidden",
-            textOverflow: "ellipsis",
-            textAlign: "center",
-            userSelect: "none",
-            fontWeight: "bolder",
-            color: "#a3a3a3",
+            borderRadius: "40px",
+            "&:has(:focus-visible)": {
+              outline: "2px solid",
+              outlineOffset: "2px",
+            },
+            margin: "8px",
           }}
         >
-          Settings
+          <Avatar
+            alt="Upload new avatar"
+            src={avatarSrc}
+            sx={{ width: "128px", height: "128px" }}
+          />
+
+          <input
+            type="file"
+            accept="image/*"
+            style={{
+              border: 0,
+              clip: "rect(0 0 0 0)",
+              height: "1px",
+              margin: "-1px",
+              overflow: "hidden",
+              padding: 0,
+              position: "absolute",
+              whiteSpace: "nowrap",
+              width: "1px",
+            }}
+            onChange={handleAvatarChange}
+          />
+        </ButtonBase>
+        <Typography
+          variant="h4"
+          component="h1"
+          sx={{
+            userSelect: "none",
+            color: "#a3a3a3",
+            fontWeight: "bolder",
+            margin: "16px",
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          {user.name} {user.lastName}
+          {user.status == "online" ? (
+            <Typography
+              variant="h4"
+              component="h2"
+              sx={{
+                userSelect: "none",
+                color: "#22c55e",
+                fontWeight: "bolder",
+                margin: "16px",
+              }}
+            >
+              •
+            </Typography>
+          ) : (
+            <Typography
+              variant="h4"
+              component="h2"
+              sx={{
+                userSelect: "none",
+                color: "#dc2626",
+                fontWeight: "bolder",
+                margin: "16px",
+              }}
+            >
+              •
+            </Typography>
+          )}
         </Typography>
       </Box>
 
---
+++ client/src/types/user.ts
@@ -0,0 +1,9 @@
+import type { UserStatus } from "./user_status";
+
+export type User = {
+  name: string;
+
+  lastName: string;
+
+  status: UserStatus;
+};
